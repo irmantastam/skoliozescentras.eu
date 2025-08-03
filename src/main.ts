35f8d911275125ef8664ba1166ac459bca826ddb
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2025-08-03 19:34:40 +0300

    Use vite-ssg to generate static site

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { ViteSSG } from 'vite-ssg'
+import './style.pcss'
+import App from './App.vue'
+import { routes } from './routes'
+
+// `export const createApp` is required instead of the original `createApp(App).mount('#app')`
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  {
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        return { top: 0 }
+      }
+    },
+  },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    router.beforeEach((to, from, next) => {
+      document.title = to.meta.title
+        ? `${to.meta.title} - Skoliozės Centras`
+        : 'Skoliozės Centras'
+      next()
+    })
+  }
+)
--- src/routes.ts
+++ src/routes.ts
@@ -1,103 +1,80 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
-const routes = [
+export const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('../pages/Home.vue'),
+    component: () => import('./pages/Home.vue'),
     meta: { title: 'INNOVAMED klinika' },
   },
   {
     path: '/komanda',
     name: 'team',
-    component: () => import('../pages/Team.vue'),
+    component: () => import('./pages/Team.vue'),
     meta: { title: 'Komanda' },
   },
   {
     path: '/nemokamas-skoliozes-testavimas',
     name: 'testing',
-    component: () => import('../pages/Testing.vue'),
+    component: () => import('./pages/Testing.vue'),
     meta: { title: 'Nemokamas skoliozės testavimas' },
   },
   {
     path: '/diagnostika',
     name: 'diagnostics',
-    component: () => import('../pages/Diagnostics.vue'),
+    component: () => import('./pages/Diagnostics.vue'),
     meta: { title: 'Diagnostika' },
   },
   {
     path: '/gydymas-schroth-metodu',
     name: 'treatment',
-    component: () => import('../pages/Treatment.vue'),
+    component: () => import('./pages/Treatment.vue'),
     meta: { title: 'Gydymas Schroth metodu' },
   },
   {
     path: '/isst-schroth-metodas-ir-organizacija',
     name: 'schroth',
-    component: () => import('../pages/Schroth.vue'),
+    component: () => import('./pages/Schroth.vue'),
     meta: { title: 'ISST Schroth metodas ir organizacija' },
   },
   {
     path: '/skoliozes-gydymo-kursai-lietuvoje',
     name: 'training',
-    component: () => import('../pages/Training.vue'),
+    component: () => import('./pages/Training.vue'),
     meta: { title: 'Skoliozės gydymo kursai Lietuvoje' },
   },
   {
     path: '/scoliteam',
     name: 'scoliteam',
-    component: () => import('../pages/Scoliteam.vue'),
+    component: () => import('./pages/Scoliteam.vue'),
     meta: { title: 'Scoliteam' },
   },
   {
     path: '/sekmes-istorijos',
     name: 'success-stories',
-    component: () => import('../pages/SuccessStories.vue'),
+    component: () => import('./pages/SuccessStories.vue'),
     meta: { title: 'Sėkmės istorijos' },
   },
   {
     path: '/klinikes-praktikos-supervizija',
     name: 'supervision',
-    component: () => import('../pages/Supervision.vue'),
+    component: () => import('./pages/Supervision.vue'),
     meta: { title: 'Klinikės praktikos supervizija' },
   },
   {
     path: '/edukacija-mokykloms-tevams',
     name: 'education',
-    component: () => import('../pages/Education.vue'),
+    component: () => import('./pages/Education.vue'),
     meta: { title: 'Edukacija mokykloms, tėvams' },
   },
   {
     path: '/schroth-skoliozes-stovykla',
     name: 'camp',
-    component: () => import('../pages/Camp.vue'),
+    component: () => import('./pages/Camp.vue'),
     meta: { title: 'Schroth skoliozės stovykla' },
   },
   {
     path: '/kontaktai',
     name: 'contact',
-    component: () => import('../pages/Contact.vue'),
+    component: () => import('./pages/Contact.vue'),
     meta: { title: 'Kontaktai' },
   },
 ]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return { top: 0 }
-    }
-  },
-})
-
-router.beforeEach((to, from, next) => {
-  document.title = to.meta.title
-    ? `${to.meta.title} - Skoliozės Centras`
-    : 'Skoliozės Centras'
-  next()
-})
-
-export default router
