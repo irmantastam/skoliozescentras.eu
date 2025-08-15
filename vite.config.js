aeb3108ef284a2116dede4b60c696b19ae47d3cd
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2025-08-15 13:01:39 +0300

    Generate sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import sitemap from 'vite-plugin-sitemap'
 
 const BASE_URL = '/'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    sitemap({
+      hostname: 'https://www.skoliozescentras.eu',
+    }),
+  ],
   base: BASE_URL,
   ssgOptions: {
     // Generate static html for 404 page.
