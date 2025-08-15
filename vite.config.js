5e6c2cd7b9d794d89cc08b1915ad6101ed95af52
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2025-08-15 13:15:58 +0300

    Use vite-ssg-sitemap to generate sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import sitemap from 'vite-plugin-sitemap'
+import generateSitemap from 'vite-ssg-sitemap'
 
 const BASE_URL = '/'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    sitemap({
-      hostname: 'https://www.skoliozescentras.eu',
-    }),
-  ],
+  plugins: [vue()],
   base: BASE_URL,
   ssgOptions: {
     // Generate static html for 404 page.
     includedRoutes(paths) {
       return paths.filter((path) => !path.includes(':')).concat('/404')
+    },
+    onFinished() {
+      generateSitemap({
+        hostname: 'https://www.skoliozescentras.eu',
+      })
     },
   },
 })
