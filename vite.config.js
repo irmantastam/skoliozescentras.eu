29fdc6b12375fc9c80cca238c08ca1ca4931167d
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2025-08-19 22:51:46 +0300

    Try using readable param on vite-ssg-sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Sitemap from 'vite-plugin-sitemap'
+import generateSitemap from 'vite-ssg-sitemap'
 
 const BASE_URL = '/'
 
-const dynamicRoutes = [
-  '/apie-mus',
-  '/komanda',
-  '/diagnostika',
-  '/nemokamas-skoliozes-testavimas',
-  '/gydymas-schroth-metodu',
-  '/sekmes-istorijos',
-  '/isst-schroth-metodas-ir-organizacija',
-  '/skoliozes-gydymo-kursai-lietuvoje',
-  '/klinikes-praktikos-supervizija',
-  '/scoliteam',
-  '/edukacija-mokykloms-tevams',
-  '/schroth-skoliozes-stovykla',
-  '/kontaktai',
-]
-
 export default defineConfig({
-  plugins: [
-    vue(),
-    Sitemap({ hostname: 'https://www.skoliozescentras.eu', dynamicRoutes }),
-  ],
+  plugins: [vue()],
   base: BASE_URL,
   ssgOptions: {
     // Generate static html for 404 page.
     includedRoutes(paths) {
       return paths.filter((path) => !path.includes(':')).concat('/404')
+    },
+    onFinished() {
+      generateSitemap({
+        hostname: 'https://www.skoliozescentras.eu',
+        exclude: ['/404'],
+        changefreq: 'monthly',
+        readable: true,
+      })
     },
   },
 })
